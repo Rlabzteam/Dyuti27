d13619ec448dbd0b60b555d3836d686463bd0ac8
Blessen P Shaju <blessenpshaju@gmail.com>
2026-09-11 06:13:10 +0530

    chore: remove unused configuration file

--- scratch/check_scripts.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const files = fs.readdirSync('.').filter(f => f.endsWith('.html'));
-files.forEach(f => {
-  const content = fs.readFileSync(f, 'utf8');
-  const hasAppJs = content.includes('js/app.js');
-  const scripts = content.match(/<script[^>]*src=["'][^"']+["'][^>]*>/g) || [];
-  console.log(`${f}: has js/app.js? ${hasAppJs}, scripts: ${scripts.join(', ')}`);
-});
